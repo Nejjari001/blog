e4daabb271961ab1f54f9e4b3da7f797d80ea754
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-09 13:28:20 +0100

    Fix linters errors

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,4 +1,5 @@
-equire 'test_helper'
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+end
