2c50c8f875d6db4fc580a3d438ec2aad86b6284f
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-09 16:09:14 +0100

    Fix linters errors

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,13 +1,12 @@
 class Comment < ApplicationRecord
-    belongs_to :author, class_name: 'User'
-    belongs_to :post
-  
-    validates :text, presence: true
-  
-    after_save :update_posts_counter
-  
-    def update_posts_counter
-      post.increment!(:comments_counter)
-    end
+  belongs_to :author, class_name: 'User'
+  belongs_to :post
+
+  validates :text, presence: true
+
+  after_save :update_posts_counter
+
+  def update_posts_counter
+    post.increment!(:comments_counter)
   end
-  
+end
