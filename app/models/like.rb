056ec747475960655901fd821dd4eab48b13f8ca
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-09 16:06:24 +0100

    Add test config details

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,10 +1,13 @@
 class Like < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  belongs_to :post
-
-  def update_like_counter
-    post.increment!(:likes_counter)
+    belongs_to :author, class_name: 'User'
+    belongs_to :post
+  
+    after_save :update_like_counter
+  
+    def update_like_counter
+      post.increment!(:likes_counter)
+    end
+  
+    private :update_like_counter
   end
-
-  private :update_like_counter
-end
+  
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,18 +1,21 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
-  has_many :comments
-  has_many :likes
-
-  validates :title, length: { in: 5..200 }
-  validates :text, length: { minimum: 5 }
-  validates :comments_counter, numericality: { only_integer: true }
-  validates :likes_counter, numericality: { only_integer: true }
-
-  def update_posts_counter
-    author.increment!(:posts_counter)
+    belongs_to :author, class_name: 'User'
+    has_many :comments
+    has_many :likes
+  
+    validates :title, length: { maximum: 250 }, presence: true
+    validates :text, length: { minimum: 5 }
+    validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+    validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  
+    after_save :update_posts_counter
+  
+    def update_posts_counter
+      author.increment!(:posts_counter)
+    end
+  
+    def most_recent_comments
+      comments.order(created_at: :desc).limit(5)
+    end
   end
-
-  def most_recent_comments
-    comments.order(created_at: :desc).limit(5)
-  end
-end
+  
