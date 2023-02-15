93e890029470cb3f07ac9fb51f104f298b87648c
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-15 15:20:53 +0100

    Add rails-controller-testing

--- spec/spec/requests/posts_spec.rb
+++ spec/spec/requests/posts_spec.rb
@@ -1,7 +1,27 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
+# RSpec.describe 'Posts', type: :request do
+#   describe 'GET /index' do
+#     before(:example) { get user_posts_path(1) }
+
+#     it 'should return a successful response' do
+#       expect(response).to have_http_status(:success)
+#     end
+
+#     it 'shoud render posts/index.html.erb template' do
+#       expect(response).to render_template('index')
+#     end
+
+#     it 'response body should contain' do
+#       expect(response.body).to include('Posts Page')
+#     end
+#   end
+
+#   describe 'GET /show' do
+#     before(:example) { get user_posts_path(1, 1) }
+
+#     it 'should return a successful response' do
+#       expect(response).to have_http_status(:success)
+#     end
+#   end
+# end
