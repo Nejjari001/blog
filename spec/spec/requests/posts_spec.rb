3b4dbf6f8806d82cc098bbafd91f85704643e3f1
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-15 15:21:54 +0100

    Add posts and user request spec files

--- spec/spec/requests/posts_spec.rb
+++ spec/spec/requests/posts_spec.rb
@@ -1,27 +1,27 @@
-# require 'rails_helper'
+require 'rails_helper'
 
-# RSpec.describe 'Posts', type: :request do
-#   describe 'GET /index' do
-#     before(:example) { get user_posts_path(1) }
+RSpec.describe 'Posts', type: :request do
+  describe 'GET /index' do
+    before(:example) { get user_posts_path(1) }
 
-#     it 'should return a successful response' do
-#       expect(response).to have_http_status(:success)
-#     end
+    it 'should return a successful response' do
+      expect(response).to have_http_status(:success)
+    end
 
-#     it 'shoud render posts/index.html.erb template' do
-#       expect(response).to render_template('index')
-#     end
+    it 'shoud render posts/index.html.erb template' do
+      expect(response).to render_template('index')
+    end
 
-#     it 'response body should contain' do
-#       expect(response.body).to include('Posts Page')
-#     end
-#   end
+    it 'response body should contain' do
+      expect(response.body).to include('Posts Page')
+    end
+  end
 
-#   describe 'GET /show' do
-#     before(:example) { get user_posts_path(1, 1) }
+  describe 'GET /show' do
+    before(:example) { get user_posts_path(1, 1) }
 
-#     it 'should return a successful response' do
-#       expect(response).to have_http_status(:success)
-#     end
-#   end
-# end
+    it 'should return a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
