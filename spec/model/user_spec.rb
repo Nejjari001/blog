2da18698205dc0bcf4bdcffa90128d6051761f9d
Abdo Nejjari <mr.nejjari.abdelhafid@gmail.com>
2023-02-25 03:01:01 +0100

    Update database.yml, Add root route and Add users request spec file

--- spec/model/user_spec.rb
+++ spec/model/user_spec.rb
@@ -1,58 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before :all do
-    @user = User.create(
+  let(:user) do
+    User.new(
       name: 'Tom',
       photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
       bio: 'Teacher from Mexico.',
-      posts_counter: 1
+      posts_counter: 5
     )
-
-    7.times.collect do
-      Post.create(
-        title: 'my post title',
-        text: 'My post text',
-        comments_counter: 0,
-        likes_counter: 0,
-        author_id: @user.id
-      )
-    end
   end
 
-  # Validations
-  describe '#validations' do
-    subject do
-      User.new(
-        name: 'Tom',
-        photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
-        bio: 'Teacher from Mexico.',
-        posts_counter: 1
-      )
-    end
+  before { subject.save }
 
-    before { subject.save }
+  it { should have_many :posts }
+  it { should have_many :comments }
+  it { should have_many :likes }
+  it { should validate_presence_of(:name) }
+  it { should validate_numericality_of(:posts_counter) }
 
-    it 'name shoud be present' do
-      subject.name = nil
-      expect(subject).to_not be_valid
-    end
-
-    it 'valid when posts_counter is less than 0' do
-      subject.posts_counter = -1
-      expect(subject).to_not be_valid
-    end
-
-    it 'valid when posts_counter is 0' do
-      subject.posts_counter = 0
-      expect(subject).to be_valid
-    end
-  end
-
-  # Methods
-  describe '#most_recent_posts' do
-    it 'should return most recent posts #3' do
-      expect(@user.most_recent_posts.length).to eq 3
-    end
+  it 'Should output 0 to 3 last comment when recent_posts is called' do
+    expect(subject.recent_post_counter.length).to be_between(0, 3)
   end
 end
